f92790c50fa1eebaa676a99f3f6be6c23522c58a
Fenrir0279 <94824143+Fenrir0279@users.noreply.github.com>
2022-12-18 14:24:38 +0530

    Update even_fib.py

--- even_fib.py
+++ even_fib.py
@@ -1,2 +1,2 @@
-print((lambda e:sum(filter(lambda q:not q%2,[(d:=lambda n:d(n-1)+d(n-2)if n>1 else n)(i)for i in range(e)])))(int(input())))
-# 124 characters
+print(sum(filter(lambda q:not q%2,[(d:=lambda n:d(n-1)+d(n-2)if n>1 else n)(i)for i in range(int(input()))])))
+# 110 characters
