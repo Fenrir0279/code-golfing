4dccbb2484b34164977e7caf8b26e81639fb46ca
Fenrir <94824143+Fenrir0279@users.noreply.github.com>
2022-12-19 21:31:46 +0530

    Update euler_6.py

--- euler_6.py
+++ euler_6.py
@@ -1,2 +1,3 @@
-print((f:=lambda n:n if n<0else(n+1)**2*n+f(n-1))(int(input())-1)+1)
-# 68 characters
+print((f:=lambda n:n if n<0else(n+1)**2*n+f(n-1))(99)+1)
+# 56 characters
+# You can change 99 to int(input())-1 if you want user input instead of a constant value. The program is 68 characters long with this change.
